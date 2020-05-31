3e8742b311a2a96fad1dea35aa27aed70c2d1e75
yanwenhui <169747926@qq.com>
2020-05-31 15:14:06 +0800

    change image name.

--- exam/docker/deploy.sh
+++ exam/docker/deploy.sh
@@ -14,8 +14,8 @@ cp docker/nginx.conf dist/nginx.conf
 cd dist
 
 echo '\n=> build docker image\n'
-docker rm -f station-website
+docker rm -f exam-web
 docker rmi -f $(docker images -q -f dangling=true)
 
-docker build -t station-website:0.1 .
-docker run -d --name exam-website -p 80:80 exam-website:0.1
+docker build -t exam-web:0.1 .
+docker run -d --name exam-web -p 80:80 exam-web:0.1
